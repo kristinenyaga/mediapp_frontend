d838af7f4c3acba124e06ca898e989f5f81e5519
krisine <kristine.nyaga@student.moringaschool.com>
2024-11-24 09:42:29 +0300

    initial setup

---
+++ app/components/welcomePage/WelcomePage.tsx
@@ -0,0 +1,15 @@
+"use client"
+import React from 'react'
+
+const WelcomePage = () => {
+  return (
+    <div className='flex justify-center items-center h-[100vh]'>
+      <div className='flex items-center gap-1'>
+        <p className='text-display-xs text-gray-700'>Welcome to</p>
+        <p className='text-display-xs text-blue-300'>MediApp</p>
+      </div>
+    </div>
+  )
+}
+
+export default WelcomePage
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,10 +9,52 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        white: '#FFFFFF',
+        'custom-bg': '#EBF0F8',
+
+        gray: {
+          100: '#FCFCFC',
+          200: '#F9F9F9',
+          300: '#DFE1E0',
+          400: '#C1C3C2',
+          500: '#6A6F6D',
+          600: '#4F5653',
+          700: '#363D3A',
+        },
+        black: '#000201',
+        brand: {
+          100: '#F3FAED',
+          200: '#E7F4DC',
+          300: '#CFE9B9',
+          400: '#A0D16E',
+          500: '#89C53F',
+          600: '#76AB35',
+        },
+        blue: {
+          100: '150 199 10',
+          200:'#0096C7',
+          300:'#0077B6'
+        },
+        red: {
+          100: '#FEEEEE',
+          200: '#FE8F83',
+          300: '#ED2B2A',
+        },
+        green: '#89C53F'
       },
-    },
+      fontSize: {
+        'display-lg': ['3rem', { lineHeight: '3.75rem', letterSpacing: '-2%' }],
+        'display-sm': ['2rem', { lineHeight: '2.5rem', letterSpacing: '-2%' }],
+        'display-xs': ['1.5rem', { lineHeight: '2rem', letterSpacing: '-2%' }],
+        'headline-lg': ['1.25rem', { lineHeight: '1.875rem', letterSpacing: '-1%' }],
+        'headline-md': ['1.125rem', { lineHeight: '1.75rem', letterSpacing: '-1%' }],
+        'headline-sm': ['1rem', { lineHeight: '1.5rem', letterSpacing: '-1%' }],
+        'body-lg': ['1.25rem', { lineHeight: '1.875rem' }],
+        'body-md': ['1rem', { lineHeight: '1.5rem' }],
+        'body-sm': ['0.875rem', { lineHeight: '1.25rem' }],
+        'body-xs': ['0.75rem', { lineHeight: '1.125rem' }],
+      }
+    }
   },
   plugins: [],
 } satisfies Config;
