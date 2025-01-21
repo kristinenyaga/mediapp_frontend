f06dcbedde17c2dfe81e8d77d3e1ca9ecdfab903
krisine <kristine.nyaga@student.moringaschool.com>
2025-01-21 15:29:15 +0300

    update the auth context

--- app/context/authContext.js
+++ app/context/authContext.js
@@ -2,25 +2,35 @@
 
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import axios from 'axios';
+import { useRole } from './RoleContext';
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-
+  const { role } = useRole()
+  console.log(role)
   const fetchUserDetails = async () => {
-    try {
-      console.log(sessionStorage.getItem('access_token'))
-      const response = await axios.get('http://localhost:5000/api/patient/profile', {
-        headers: { Authorization: `Bearer ${sessionStorage.getItem('access_token')}` },
-      });
-      const { id, email, username } = response.data; 
-      setUser({ id, email, username });
-    } catch (error) {
-      console.error('Error fetching user details:', error);
-      setUser(null);
-    }
-  };
+  try {
+    if (!role) return;
+
+    const endpoint =
+      role === "doctor"
+        ? "http://localhost:5000/api/doctor/profile"
+        : "http://localhost:5000/api/patient/profile";
+
+    const response = await axios.get(endpoint, {
+      headers: { Authorization: `Bearer ${sessionStorage.getItem("access_token")}` },
+    });
+
+    const { id, email, username } = response.data;
+    setUser({ id, email, username });
+  } catch (error) {
+    console.error("Error fetching user details:", error);
+    setUser(null);
+  }
+};
+
 
   useEffect(() => {
     const token = sessionStorage.getItem('access_token');
