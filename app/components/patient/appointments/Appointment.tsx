f09a660b765a6836830c804684129df102bf203b
krisine <kristine.nyaga@student.moringaschool.com>
2025-01-23 20:50:15 +0300

    update styling on appointments page

--- app/components/patient/appointments/Appointment.tsx
+++ app/components/patient/appointments/Appointment.tsx
@@ -30,27 +30,27 @@ const Appointment = () => {
     <PatientLayout>
       <div className="">
         <h1 className="text-2xl font-medium text-blue-600"> Appointments</h1>
-        <p className='text-gray-500 mb-7'>These are all you scheduled appointments</p>
+        <p className='text-gray-500 mb-7'>These are all your appointments</p>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          {appointments.map((appointment) => (
+          {appointments.map((appointment,index) => (
             <div
               key={appointment.id}
               className="cursor-pointer bg-white rounded-lg p-4 border border-gray-300 hover:border-blue-500 transition duration-200 ease-in-out"
               onClick={() => handleAppointmentClick(appointment.id)}
             >
               <div className="flex flex-col">
-                <h2 className="text-lg font-semibold text-gray-800">{appointment.doctor.username}</h2>
-                <p className="text-sm text-gray-500">Date: {appointment.date}</p>
-                <p className="text-sm text-gray-500">Time: {appointment.startTime} - {appointment.endTime}</p>
+                <h2 className="text-[18px] font-medium text-gray-700">Appointment {index+1}</h2>
+                <p className="text-sm text-blue-600 font-medium mt-3">Date: {appointment.date}</p>
+                <p className="text-sm mt-2 text-gray-600 font-medium">Time: {appointment.startTime} - {appointment.endTime}</p>
                 <p
-                  className={`text-sm font-semibold ${appointment.status === 'pending'
+                  className={`text-sm mt-2 border w-[100px] pl-5 p-2 rounded-full bg-yellow-50 font-semibold ${appointment.status === 'pending'
                       ? 'text-yellow-600'
                       : appointment.status === 'confirmed'
                         ? 'text-green-600'
                         : 'text-red-600'
                     }`}
                 >
-                  Status: {appointment.status}
+                  {appointment.status}
                 </p>
               </div>
             </div>
