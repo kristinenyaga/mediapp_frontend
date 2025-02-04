995df55ac133e25873e212106f39b9ae2c4a19e1
krisine <kristine.nyaga@student.moringaschool.com>
2025-02-04 14:20:02 +0300

    changed the entry to the app

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
+import LandingPage from "./components/welcomePage/landingPage/LandingPage";
 import WelcomePage from "./components/welcomePage/WelcomePage";
 
 export default function Home() {
   return (
-    <WelcomePage />
+    <LandingPage />
   );
 }
