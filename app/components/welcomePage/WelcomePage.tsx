6b3d35daaf1f1f89c1c4d9b8ab4f96375696264d
krisine <kristine.nyaga@student.moringaschool.com>
2025-02-06 13:27:41 +0300

    changed the styling

--- app/components/welcomePage/WelcomePage.tsx
+++ app/components/welcomePage/WelcomePage.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import Image from "next/image";
 import React from "react";
 import { useRole } from "@/app/context/RoleContext";
 import { useRouter } from "next/navigation";
-import { doctor, patient } from "@/public/constants/images";
+import { doctor, patient, stethoscope } from "@/public/constants/images";
+import Image from "next/image";
 
 const WelcomePage = () => {
   const { setRole } = useRole();
@@ -16,29 +16,43 @@ const WelcomePage = () => {
   };
 
   return (
-    <div className="flex flex-col gap-3 justify-center items-center h-[80vh]">
-      <div className="flex items-center gap-1">
-        <p className="text-display-xs text-gray-700">Welcome to</p>
-        <p className="text-display-xs text-blue-300">MediApp</p>
-      </div>
-      <p className="text-gray-500">Select your role</p>
-      <div className="flex gap-10 mt-10">
-        <div
-          className="border border-[#C1C3C2] rounded-md py-5 px-10 cursor-pointer"
-          onClick={() => handleRoleSelection("patient")}
-        >
-          <Image src={patient} alt="Patient Icon" />
-          <p className="text-center pt-2 text-gray-500">Patient</p>
+    <div>
+      <p className="px-10 pt-10">Mediapp</p>
+      <div className="relative h-screen flex flex-col items-center mt-[6%] px-6">
+
+        <Image src={stethoscope} alt='stethoscope' className="absolute text-[#6B4DE6] text-3xl top-10 left-[20%] animate-pulse" />
+
+        <div className="text-center mb-16">
+          <h1 className="text-5xl font_dm_serif mb-3 text-[#6B4DE6] tracking-wide">MediApp</h1>
+          <p className="text-gray-600 text-sm mt-2 ">Your trusted medical companion</p>
         </div>
-        <div
-          className="border border-[#C1C3C2] rounded-md py-5 px-10 cursor-pointer"
-          onClick={() => handleRoleSelection("doctor")}
-        >
-          <Image src={doctor} alt="Doctor Icon" />
-          <p className="text-center pt-2 text-gray-500">Doctor</p>
+
+        {/* Role Selection Section */}
+        <p className="text-gray-600 text-base mb-6">Choose your role</p>
+        <div className="flex gap-10">
+          {/* Patient Card */}
+          <div
+            className="bg-[#ffffff] shadow-md hover:shadow-lg py-8 px-16 cursor-pointer transition-all duration-300 transform hover:scale-105 flex flex-col items-center rounded-lg border border-gray-200"
+            onClick={() => handleRoleSelection("patient")}
+          >
+            <Image src={patient} alt="Patient Icon" width={60} height={60} />
+            <p className="text-gray-600 text-lg mt-3">Patient</p>
+          </div>
+
+          {/* Doctor Card */}
+          <div
+            className="bg-white shadow-md hover:shadow-lg py-8 px-14 cursor-pointer transition-all duration-300 transform hover:scale-105 flex flex-col items-center rounded-lg border border-gray-200"
+            onClick={() => handleRoleSelection("doctor")}
+          >
+            <Image src={doctor} alt="Doctor Icon" width={60} height={60} />
+            <p className="text-gray-600 text-lg  mt-3">Doctor</p>
+          </div>
         </div>
+
+        <p className="text-gray-600 text-sm mt-10">Providing seamless healthcare experiences</p>
       </div>
     </div>
+
   );
 };
 
