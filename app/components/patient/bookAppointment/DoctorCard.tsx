8e65eb14f0b88418d38d169e90b5e519024f9658
krisine <kristine.nyaga@student.moringaschool.com>
2025-02-07 13:17:16 +0300

    added the loading functionality

--- app/components/patient/bookAppointment/DoctorCard.tsx
+++ app/components/patient/bookAppointment/DoctorCard.tsx
@@ -1,14 +1,19 @@
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
+import LoadingScreen from "../../loader/Loader";
 const DoctorCard = ({ doctor, onSelect }) => {
   const router = useRouter()
+  const [isLoading, setIsLoading] = useState(false)
+  if(isLoading) return <LoadingScreen />
   return (
     <div
       className="border border-gray-300 shadow-sm rounded-lg p-4 cursor-pointer hover:shadow-md transition"
       onClick={() => {
+        setIsLoading(true)
         router.push(`/patient/doctor-details/${doctor.id}`)
         onSelect(doctor.id)
+        setIsLoading(false)
       }
       }
     >
@@ -20,8 +25,8 @@ const DoctorCard = ({ doctor, onSelect }) => {
           className="w-16 h-16 rounded-full border border-gray-200 mr-4"
         /> */}
         <div>
-          <p className="text-[18px] font-medium text-gray-700">{doctor.username}</p>
-          <p className="text-[14px] text-blue-600 font-medium">{doctor.specialty || "General Practitioner"}</p>
+          <p className="text-[18px] font-medium text-gray-600">{doctor.username}</p>
+          <p className="text-[14px] text-secondary font-medium">{doctor.specialty || "General Practitioner"}</p>
         </div>
       </div>
 
@@ -30,11 +35,11 @@ const DoctorCard = ({ doctor, onSelect }) => {
           <p className="text-sm text-gray-500">
             <span className="font-medium">Experience:</span> {doctor.experience || "N/A"} years
           </p>
-          <p className="text-sm text-gray-500">
-            <span className="font-medium">Availability:</span> {doctor.availability || "Contact for details"}
+          <p className="text-sm text-brand-600">
+            <span className="font-medium">Availability: <span className="text-gray-600 font-medium">{doctor.availability || "Mon - Fri"}</span></span> 
           </p>
         </div> 
-        <p className="lg:mr-2 bg-blue-600 flex justify-center items-center py-1.5 rounded-md text-white px-4">
+        <p className="lg:mr-2 bg-secondary flex justify-center items-center py-1.5 rounded-md text-white px-4">
           select
         </p>
       </div>
